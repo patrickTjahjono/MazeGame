543d2110c24e1f129b8f62866e2a9369b30b1974
Derek Lin <ys.lin113@gmail.com>
2021-03-17 00:10:45 -0700

    add function to calculate distances between two cells given their positions

--- MazeGame/src/main/java/BoardState.java
+++ MazeGame/src/main/java/BoardState.java
@@ -49,6 +49,20 @@ public class BoardState {
         return(this.boardState[x][y].getIsSolid());
     }
 
+    public int calculateSquaredDistance(int x1, int y1, int x2, int y2) {
+        int squaredDistance;
+        int cell1IsSolid = getCellIsSolid(x1, y1);
+        int cell2IsSolid = getCellIsSolid(x2, y2);
+        if ((cell1IsSolid == 1) || (cell2IsSolid == 1)) {
+            squaredDistance = Integer.MAX_VALUE;
+        } else {
+            int differenceX = (x2 - x1);
+            int differenceY = (y2 - y1);
+            squaredDistance = differenceX * differenceX + differenceY * differenceY;
+        }
+        return squaredDistance;
+    }
+
     public void updateBoard(Player player) {
         for (int y = 0; y < this.height; y++) {
             for (int x = 0; x < this.width; x++) {
