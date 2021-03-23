8e33c8c2544fac88de1110c2a7c76867803d8c72
Derek Lin <ys.lin113@gmail.com>
2021-03-22 20:42:41 -0700

    adapt Rewards from BR_SB branch, add image for reward

---
+++ MazeGame/src/main/java/Rewards.java
@@ -0,0 +1,33 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+public class Rewards extends Position{
+    JLabel label;
+
+    public Rewards(int x, int y) throws IOException {
+        super(x, y);
+        Image image = ImageIO.read(new File("src/reward.png"));
+        this.label = new JLabel(new ImageIcon(new ImageIcon(image).getImage().getScaledInstance(25, 25, Image.SCALE_FAST)));
+
+        Board board = Board.getInstance();
+        board.getCells()[x][y].add(label);
+    }
+
+    public JLabel getLabel() {
+        return label;
+    }
+
+    public void collectedRR() {
+        BoardState boardState = BoardState.getInstance();
+        if(boardState.boardStateCells[this.getX()][this.getY()].getContainsReward() == 1) {
+            ScoreBoard scoreBoard = ScoreBoard.getInstance();
+            scoreBoard.updateScore(5);
+            Board board = Board.getInstance();
+            board.getCells()[this.getX()][this.getY()].remove(this.getLabel());
+        }
+    }
+}
+
