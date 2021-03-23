ffefd447b0384f401885771f50f2f00d32bbbe8c
Derek Lin <ys.lin113@gmail.com>
2021-03-22 20:41:11 -0700

    add attributes containsReward, containsBonusReward, containsPunishment along with their getters and setters

--- MazeGame/src/main/java/Cell.java
+++ MazeGame/src/main/java/Cell.java
@@ -1,9 +1,15 @@
 public class Cell extends Position {
     private int isSolid;
+    private int containsReward;
+    private int containsBonusReward;
+    private int containsPunishment;
 
     public Cell(int x, int y, int isSolid) {
         super(x, y);
         this.isSolid = isSolid;
+        this.containsReward = 0;
+        this.containsBonusReward = 0;
+        this.containsPunishment = 0;
     }
 
     public int getIsSolid() {
@@ -12,5 +18,37 @@ public class Cell extends Position {
 
     public void setIsSolid(int isSolid) {
         this.isSolid = isSolid;
+    }
+
+    public int getContainsReward() {
+        return containsReward;
+    }
+
+    public void setContainsReward(int containsReward) {
+        this.containsReward = containsReward;
+    }
+
+    public int getContainsBonusReward() {
+        return containsBonusReward;
+    }
+
+    public void setContainsBonusReward(int containsBonusReward) {
+        this.containsBonusReward = containsBonusReward;
+    }
+
+    public int getContainsPunishment() {
+        return containsPunishment;
+    }
+
+    public void setContainsPunishment(int containsPunishment) {
+        this.containsPunishment = containsPunishment;
+    }
+
+    public int getContainsRewardOrPunishment() {
+        if (this.getContainsReward() == 1 || this.getContainsBonusReward() == 1 || this.getContainsPunishment() == 1) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
