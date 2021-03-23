fedca67547d3aaa0f3f4d3bdcfcc8f0394bd360d
syy7 <felicity_yang@sfu.ca>
2021-03-23 05:30:38 -0700

    added integer continue_game for player status on board

--- MazeGame/src/main/java/Board.java
+++ MazeGame/src/main/java/Board.java
@@ -5,6 +5,7 @@ import javax.swing.border.Border;
 public class Board extends JPanel {
     private static Board instance = null;
     private JPanel[][] cells;
+    public int continue_game;   // check status of player
 
     public static Board getInstance() {
         if (instance == null)
@@ -13,6 +14,7 @@ public class Board extends JPanel {
     }
 
     private Board() {
+        continue_game = 1;
         JPanel[][] cells = new JPanel[20][10]; // 20 cell wide, 10 cell tall board
         setLayout(new GridLayout(10, 20));
         Dimension cellDimensions = new Dimension(32, 32); // 32px by 32px cell size
