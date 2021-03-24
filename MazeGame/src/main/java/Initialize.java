6151091a72e1dabadbacf929549ab2f558ad8764
ptjahjon <patrick_tjahjono@sfu.ca>
2021-03-23 21:30:06 -0700

    windows closer fixed

--- MazeGame/src/main/java/Initialize.java
+++ MazeGame/src/main/java/Initialize.java
@@ -28,12 +28,7 @@ public class Initialize {
             MazeFrame.addWindowListener(new java.awt.event.WindowAdapter() {
                 @Override
                 public void windowClosing(java.awt.event.WindowEvent windowEvent) {
-                    if (JOptionPane.showConfirmDialog(MazeFrame,
-                            "Are you sure you want to close this window?", "Close Window?",
-                            JOptionPane.YES_NO_OPTION,
-                            JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION){
                         System.exit(0);
-                    }
                 }
             });
             player1 = Player.getInstance();
