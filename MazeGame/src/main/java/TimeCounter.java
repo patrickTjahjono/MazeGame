d883a8a1d205091bab02742da732e0c3b67af09c
handah <handa_huang@sfu.ca>
2021-03-24 02:04:15 -0700

    added javadoc

--- MazeGame/src/main/java/TimeCounter.java
+++ MazeGame/src/main/java/TimeCounter.java
@@ -1,12 +1,21 @@
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ *
+ * TimeCounter class creates the GUI of the TimeCounter component to display the time of the game.
+ * The game's time and turn will increment at each update of the time.
+ */
 public class TimeCounter {
     private static TimeCounter instance;
     private float time;
     private int turn;
     JLabel label;
 
+
+/**
+ * Creates a new TimeCounter used as the GUI display for the game's time. Initial time and turn are both set to 0.
+ */
     private TimeCounter() {
         turn = 0;
         time = 0;
@@ -37,7 +46,9 @@ public class TimeCounter {
         label.setForeground(Color.WHITE);
         Board.getInstance().getCells()[15][0].add(label);
     }
-
+    /**
+     * @return the existing instance of TimeCounter or a new instance of counter if one does not exist.
+     */
     public static TimeCounter getInstance() {
         if (instance == null) {
             instance = new TimeCounter();
@@ -45,6 +56,10 @@ public class TimeCounter {
         return instance;
     }
 
+
+    /**
+     * Updates the global time value, turn value and TimeCounter GUI.
+     */
     public void updateTime() {
         this.time += 0.5;
         turn += 1;
@@ -56,10 +71,18 @@ public class TimeCounter {
         Board.getInstance().getCells()[15][0].add(label);
     }
 
+    /**
+     *
+     * @return the time stored on TimeCounter.
+     */
     public float getTime() {
         return this.time;
     }
 
+    /**
+     *
+     * @return the turn stored on TimeCounter.
+     */
     public int getTurn() {
         return turn;
     }
