e6420372911c3f3dcff606080c56b680931a1964
syy7 <felicity_yang@sfu.ca>
2021-03-24 00:04:23 -0700

    added javadoc

--- MazeGame/src/main/java/Punishment.java
+++ MazeGame/src/main/java/Punishment.java
@@ -4,9 +4,22 @@ import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * 
+ * The Punishment class creates the GUI object for punishment and position the obejct on board. 
+ * If player's position collides with punishment's position, punishment JLabel gets removed from board, and scoreBoard gets updated.
+ */
 public class Punishment extends Position{
     private JLabel label;
 
+    /**
+     * 
+     * Creates a new Punishment on Board at (x, y) position. 
+     * 
+     * @param x the x coordinate that the punishment object to be placed on, range from [0, 19].
+     * @param y the y coordinate that the punihsment object to be placed on, range from [0, 20].
+     * @throws IOException if (x, y) coordinate is out of range.
+     */
     public Punishment(int x, int y) throws IOException {
         super(x, y);
         Image image = ImageIO.read(new File("src/punishment.png"));
@@ -16,10 +29,19 @@ public class Punishment extends Position{
         board.getCells()[x][y].add(label);
     }
 
+    /**
+     * 
+     * @return JLabel of the Punishment object.
+     */
     public JLabel getLabel() {
         return label;
     }
 
+    /**
+     * Gets the intance of BoardState and checks if Puishment is collected by the player.
+     * If punishment is collected, the method gets the instance of the ScoreBoard and Board. 
+     * Player's score gets updated (-5), collected punishment's JLabel gets removed from cell.
+     */
     public void collectedPunishment() {
         BoardState boardState = BoardState.getInstance();
         if(boardState.boardStateCells[this.getX()][this.getY()].getContainsPunishment() == 1) {
--- MazeGame/src/main/java/Rewards.java
+++ MazeGame/src/main/java/Rewards.java
@@ -4,9 +4,20 @@ import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * 
+ * The Rewards class creates the GUI object for regular rewards and position obejcts on board. 
+ * If Rewards object is collected by the player, it will be removed from board, and scoreBoard gets updated.
+ */
 public class Rewards extends Position{
     private JLabel label;
 
+    /**
+     * 
+     * @param x the x coordinate that the reward object to be placed on, range from [0, 19].
+     * @param y the y coordinate that the reward object to be placed on, range from [0, 20].
+     * @throws IOException if (x, y) coordinate is out of range.
+     */
     public Rewards(int x, int y) throws IOException {
         super(x, y);
         Image image = ImageIO.read(new File("src/reward.png"));
@@ -16,10 +27,19 @@ public class Rewards extends Position{
         board.getCells()[x][y].add(label);
     }
 
+    /**
+     * 
+     * @return the JLabel of the reward object.
+     */
     public JLabel getLabel() {
         return label;
     }
 
+    /**
+     * Gets the intance of BoardState and checks if regular reward is collected by the player.
+     * If reward is collected, the method gets the instance of the ScoreBoard and Board. 
+     * Player's score gets updated, collected reward's JLabel gets removed from cell.
+     */
     public void collectedRR() {
         BoardState boardState = BoardState.getInstance();
         if(boardState.boardStateCells[this.getX()][this.getY()].getContainsReward() == 1) {
